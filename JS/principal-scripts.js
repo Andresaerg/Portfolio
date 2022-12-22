1b08fa48293f05318eb4f9900bc326d9568c4179
Andres <andrescod10@gmail.com>
2022-12-22 16:45:24 -0400

    Actualizaciones

---
+++ JS/principal-scripts.js
@@ -0,0 +1,39 @@
+const prueba = (txt, year) => {
+        setInterval(() => {
+                for(let i = 0; i < 1; i++){
+                    const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+                    txt.style.setProperty('--txtcolor-casual', randomColor)
+                }
+            }, 750
+        )
+        let date = new Date().getFullYear();
+        year.innerHTML = date;
+    }
+
+const change_view_1 = (layout1, layout2, bg, container, txt) => {
+    layout1.style.background = 'var(--bg-principal)';
+    layout2.style.background = 'var(--bg-principal)';
+    bg.style.background = 'var(--bg-body)';
+    container.forEach(element => {
+        element.style.background = 'var(--container)';
+    });
+    txt.forEach(e => {
+        e.style.color = 'black';
+    })
+
+    document.querySelectorAll('.arcodev, .eslogan, #powered, #date').forEach(e => {
+        e.style.setProperty('color', '#fff');
+    });
+}
+    
+const change_view_2 = (layout1, layout2, bg, container, txt) => {
+    layout1.style.background = 'var(--bg-secondary)';
+    layout2.style.background = 'var(--bg-secondary)';
+    bg.style.background = 'black';
+    container.forEach(element => {
+        element.style.background = 'darkgrey';
+    });
+    txt.forEach(e => {
+        e.style.color = 'white';
+    })
+}
