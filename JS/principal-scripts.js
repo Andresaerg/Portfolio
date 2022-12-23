d7d98748038571f829f26ea96ad5c48193285495
Andrés Rosales <Andrescod10@gmail.com>
2022-12-23 01:24:03 -0400

    Actualizaciones

--- JS/principal-scripts.js
+++ JS/principal-scripts.js
@@ -16,24 +16,35 @@ const change_view_1 = (layout1, layout2, bg, container, txt) => {
     bg.style.background = 'var(--bg-body)';
     container.forEach(element => {
         element.style.background = 'var(--container)';
+        element.style.border = 'var(--border)';
     });
     txt.forEach(e => {
-        e.style.color = 'black';
+        e.style.color = 'var(--article-color)';
     })
+
+    document.querySelector('.ing').style.color = 'unset';
 
     document.querySelectorAll('.arcodev, .eslogan, #powered, #date').forEach(e => {
         e.style.setProperty('color', '#fff');
     });
+
+    let paragraphs = document.querySelector('.section p');
+    paragraphs.style.color = 'var(--article-color)';
 }
     
 const change_view_2 = (layout1, layout2, bg, container, txt) => {
     layout1.style.background = 'var(--bg-secondary)';
     layout2.style.background = 'var(--bg-secondary)';
-    bg.style.background = 'black';
+    bg.style.background = 'var(--bg-body2)';
     container.forEach(element => {
-        element.style.background = 'darkgrey';
+        element.style.background = 'var(--container2)';
+        element.style.border = 'var(--border2)';
     });
     txt.forEach(e => {
-        e.style.color = 'white';
+        e.style.color = 'var(--txtcolor-secondary)';
     })
+
+    let paragraphs = document.querySelector('.section p');
+    paragraphs.style.color = 'var(--txtcolor-secondary)';
+    document.querySelector('.ing').style.color = 'var(--txtcolor-secondary)';
 }
