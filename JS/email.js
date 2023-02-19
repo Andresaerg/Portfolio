42835f37e489f48e7fcd86ff3d08f336545aa1ba
Andrés Rosales <Andrescod10@gmail.com>
2023-02-19 04:38:25 -0400

    Finalizado

--- JS/email.js
+++ JS/email.js
@@ -4,41 +4,86 @@
     let message = document.getElementById('message');
     let company = document.getElementById('company');
 
-    document.getElementById('form')
-    .addEventListener('submit', function(event) {
+    let body = document.querySelector('body');
+
+    if(body.id === 'esp'){
+
+        document.getElementById('form')
+        .addEventListener('submit', function(event) {
+            
+            event.preventDefault();
+
+            if(user_name.value === '' || email.value === '' || message.value === ''){
+                swal('Complete todos los campos', 'Por favor, complete todos los campos para enviar el mensaje', 'info', {
+                    button: 'Volver'
+                })
+            }else{
+                btn.disabled = true;
+                btn.value = 'Enviando...';
         
-        event.preventDefault();
-
-        if(user_name.value === '' || email.value === '' || message.value === ''){
-            swal('Complete todos los campos', 'Por favor, complete todos los campos para enviar el mensaje', 'info', {
-                button: 'Volver'
-            })
-        }else{
-            btn.disabled = true;
-            btn.value = 'Enviando...';
-    
-            const serviceID = 'default_service';
-            const templateID = 'template_i02kfup';
-    
-            emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Enviar';
-                swal('Correo enviado', '¡Gracias por su mensaje!', 'success', {
-                    button: 'Volver'
+                const serviceID = 'default_service';
+                const templateID = 'template_i02kfup';
+        
+                emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    btn.value = 'Enviar';
+                    swal('Correo enviado', '¡Gracias por su mensaje!', 'success', {
+                        button: 'Volver'
+                    });
+                    user_name.value = '';
+                    email.value = '';
+                    message.value = '';
+                    company.value = '';
+                    btn.disabled = false;
+                }, (err) => {
+                    btn.value = 'Enviar';
+                    console.log(JSON.stringify(err))
+                    swal('Oops', 'Algo ha salido mal...', 'error', {
+                        button: 'Volver'
+                    });
+                    btn.disabled = false;
                 });
-                user_name.value = '';
-                email.value = '';
-                message.value = '';
-                company.value = '';
-                btn.disabled = false;
-            }, (err) => {
-                btn.value = 'Enviar';
-                console.log(JSON.stringify(err))
-                swal('Oops', 'Algo ha salido mal...', 'error', {
-                    button: 'Volver'
-                });
-                btn.disabled = false;
-            });
-        }
+            }
 
-    });
+        });
+    }else{
+
+        document.getElementById('form')
+        .addEventListener('submit', function(event) {
+            
+            event.preventDefault();
+
+            if(user_name.value === '' || email.value === '' || message.value === ''){
+                swal('Complete all fields', 'Please complete all fields to send the message', 'info', {
+                    button: 'Return'
+                })
+            }else{
+                btn.disabled = true;
+                btn.value = 'Sending...';
+        
+                const serviceID = 'default_service';
+                const templateID = 'template_i02kfup';
+        
+                emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    btn.value = 'Send';
+                    swal('Mail sent', 'Thanks for your message!', 'success', {
+                        button: 'Return'
+                    });
+                    user_name.value = '';
+                    email.value = '';
+                    message.value = '';
+                    company.value = '';
+                    btn.disabled = false;
+                }, (err) => {
+                    btn.value = 'Send';
+                    console.log(JSON.stringify(err))
+                    swal('Oops', 'Something has gone wrong...', 'error', {
+                        button: 'Return'
+                    });
+                    btn.disabled = false;
+                });
+            }
+
+        });
+    }
